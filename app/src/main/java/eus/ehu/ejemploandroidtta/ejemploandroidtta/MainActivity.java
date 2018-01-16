201b6705949acc673b76314f846e4aa6459e1bfd
David Franco <dfranco009@ikasle.ehu.eus>
2018-01-16 13:31:21 +0100

    Connection with server to get user

--- app/src/main/java/eus/ehu/ejemploandroidtta/ejemploandroidtta/MainActivity.java
+++ app/src/main/java/eus/ehu/ejemploandroidtta/ejemploandroidtta/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import modelo.*;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    public School school = new School("http://u017633.ehu.eus:28080/ServidorTta/rest/tta");
+    public static User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +31,21 @@ public class MainActivity extends AppCompatActivity {
         String passwd = ((EditText)findViewById(R.id.passwd)).getText().toString();
 
         if( authenticate(login, passwd) ) {
-            intent.putExtra(EvaluationActivity.EXTRA_LOGIN,login);
-            startActivity(intent);
+
+            new ProgressTask<User>(this){
+                @Override
+                protected User work() throws Exception{
+                    return school.getUser("12345678A");
+                }
+                @Override
+                protected void onFinish(User result){
+                    MainActivity.user=result;
+                    Intent intent = new Intent(this.context, EvaluationActivity.class);
+                    //intent.putExtra(EvaluationActivity.EXTRA_LOGIN,login);
+                    startActivity(intent);
+                }
+            }.execute();
+
         }
         else {
             Toast.makeText(this, "Error de acceso", Toast.LENGTH_SHORT).show();
